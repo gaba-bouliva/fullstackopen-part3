c2da39894165af704d3a1797b131adfd3f8ea0fc
gaba-bouliva <eaglebald111@gmail.com>
2023-08-15 04:32:39 +0100

    complete phonebook db exercise 3.15 - 3.18

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -1,55 +1,28 @@
+
+require('dotenv').config()
+const Person = require('./models/person')
 const express = require('express')
 var morgan = require('morgan')
 const cors = require('cors')
+const request = require('http').request
+
 
 const app = express()
+app.use(express.static('build'))
 app.use(express.json())
 app.use(cors())
-app.use(express.static('build'))
-
+require('http')
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :param'));
 
 morgan.token('param', function(req, res) {
     return JSON.stringify(req.body);
 });
 
-
-
-let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
-
-const generateId = () => {
-    return Math.floor(Math.random() * Date.now())
-}
-
-const verifyUniqueName = (name) => {
-  const result = persons.find( person => person.name.toLowerCase() === name)
-  if (result) return false
-  return true
-}
-
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+  Person.find({}).then(persons => { 
+    console.log("People contacts: ", persons);
+    res.json(persons)
+  })
 })
 
 app.get('/info', (req, res) => {
@@ -57,51 +30,82 @@ app.get('/info', (req, res) => {
   res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${reqDate}</p>`)
 })
 
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
 
   const id = Number(req.params.id)
-  const person = persons.find( person => person.id === id )
+  const person = Person.findById(req.params.id).then(person => { 
 
-  if (!person) {
-    res.status(404).end()
-  } else {
-    res.json(person)
-  }
+    if (!person) {
+      res.status(404).end()
+    } else {
+      res.json(person)
+    }
+
+  }).catch(error => next(error))
 
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter( person => person.id !== id)
-  res.status(204).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id).then( result => {
+    res.status(204).end()
+  }).catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body
+  
+  const person = {
+    name: body.name,
+    number: body.number,
+  } 
+
+  Person.findByIdAndUpdate(req.params.id, person, {new: true}).then( updatedPerson => {
+    res.json(updatedPerson)
+  }).catch(error => next(error))
 })
 
 app.post('/api/persons', (req, res) => {
-  const person = req.body
-  
-    if (person && person.hasOwnProperty('name') && person.hasOwnProperty('number')) {
-      
-      if(verifyUniqueName(person.name.toLowerCase())) {
-        const newPerson = {
-          id: generateId(),
-          name: person.name,
-          number: person.number
-        }
-        persons.push(newPerson)
-        res.json(person)
-
-      }else {
-        res.status(400).send({ error: 'name must be unique' })
-      }
-      
-    } else {
+  const newPerson = req.body
+    
+    if (!newPerson || !newPerson.hasOwnProperty('name') || !newPerson.hasOwnProperty('number')) {
       res.status(400).send({error: 'name and number are required'})
+      return
     }
+
+    const person = new Person({
+      name: newPerson.name,
+      number: newPerson.number
+    })
+    
+    person.save().then(savedPerson => {
+      res.json(savedPerson)
+    })
 
   
 })
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+}
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'Invalid id' })
+  } 
+
+  next(error)
+}
+
+// handler of requests with result to errors
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
